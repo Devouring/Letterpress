59896dce164e3c83a11f8d74e4227e8da7d2720b
matthieu.demus@gmail.com <L0lc4t>
2013-07-07 01:05:24 +0200

    JS ans CSS improvement centered style letter coloration

--- app/assets/javascripts/my.js
+++ app/assets/javascripts/my.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-	if($("#autoresfresh_check").attr('checked')){
-		alert("tot");
-	}
+	$('.accordion-toggle').click(function(){
+		array_of_letters = $.trim($(this).text()).toUpperCase().split('');
+		$('#container').children().each(function(){
+			$(this).addClass('normalTile');
+			$(this).removeClass('blueTile');
+			for(i = 0 ; i < array_of_letters.length ; i ++){
+				if($.trim($(this).text()) == array_of_letters[i]){
+					$(this).addClass('blueTile');
+					$(this).removeClass('normalTile');
+					array_of_letters[i] = 0;
+					i = 50;
+				}
+			}
+		});		
+	});
 });

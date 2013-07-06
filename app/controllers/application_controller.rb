97f99c2a78692f3bf57d5452eb379b7350d6e2e8
matthieu.demus@gmail.com <L0lc4t>
2013-07-06 19:26:01 +0200

    everybody is admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def has_role?(current_user, role)
-    return !!current_user.roles.find_by_name(role.to_s.camelize)
+     current_user.roles.find_by_name(role.to_s.camelize)
   end
 
   rescue_from CanCan::AccessDenied do |exception|

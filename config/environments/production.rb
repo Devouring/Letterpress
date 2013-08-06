cac105bbf6cd7cf11b1afddff13607fa662b0b4b
matthieu.demus@gmail.com <L0lc4t>
2013-08-06 22:55:18 +0200

    precompile js for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ App::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.assets.precompile += %w( game.show.js games.index.js )
+
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
